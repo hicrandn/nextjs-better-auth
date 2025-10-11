03bbf5ee769e2d7faea72a1d3c3b2ec2b06c7772
Hicran Apaydın <hicran.apaydn@gmail.com>
2025-10-11 15:21:43 +0300

    feat: implementing forms and validation

--- src/server/users.ts
+++ src/server/users.ts
@@ -3,12 +3,21 @@
 import { auth } from "@/lib/auth";
 
 export const signIn = async (email: string, password: string) => {
-  await auth.api.signInEmail({
-    body: {
-      email,
-      password,
-    },
-  });
+  try {
+    await auth.api.signInEmail({
+      body: {
+        email,
+        password,
+      },
+    });
+    return { success: true, message: "Signed in successfully" };
+  } catch (error) {
+    const e = error as Error;
+    return {
+      success: false,
+      message: { error: e.message || "Invalid email or password" },
+    };
+  }
 };
 
 export const signUp = async () => {
